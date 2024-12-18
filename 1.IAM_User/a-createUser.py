0162951750176541d0d07696ce5b60b4c39397df
Tam Nguyen <tamng.tng@gmail.com>
2024-12-18 15:36:27 +0700

    add code

--- 1.IAM_User/a-createUser.py
+++ 1.IAM_User/a-createUser.py
@@ -4,14 +4,13 @@ iam_client = boto3.client('iam')
 
 def create_user(username):
     try:
-        response = iam_client.create_user(UserName=username)
-        print("Người dùng đã được tạo thành công:", response)
+        response = iam_client.create_user(UserName = username)
+        print("Người dùng đã được tạo thành công: ", response)
     except iam_client.exceptions.EntityAlreadyExistsException:
-        print("Người dùng đã tồn tại.")
+        print("Người dùng này đã tồn tại!")
     except Exception as e:
-        print("Đã xảy ra lỗi:", e)
-        
-if __name__ == "__main__":
-    username = input("Enter the username: ")
-    create_user(username)
+        print("Đã xảy ra lỗi!", e)
 
+if __name__ == "__main__":
+    username = input("Nhập tên người dùng: ")
+    create_user(username)
--- 1.IAM_User/b-listUser.py
+++ 1.IAM_User/b-listUser.py
@@ -3,8 +3,11 @@ import boto3
 iam_client = boto3.client('iam')
 
 def list_users():
-    response = iam_client.list_users()
-    for user in response['Users']:
-        print("Username: ", user['UserName'], '-', "ARN: ", user['Arn'] )
+    try:
+        response = iam_client.list_users()
+        for user in response['Users']:
+            print("Username: ", user['UserName'], ' - ', "ARN: ", user['Arn'])
+    except iam_client.exceptions.ServiceFailureException:
+        print("Dịch vụ đang bị lỗi, bạn thử lại sau nhé!")
 if __name__ == "__main__":
     list_users()
